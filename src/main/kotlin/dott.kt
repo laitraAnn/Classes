4a4252bccf45929595c37af5115a6e42ad1dab3b
laitraAnn <162510014+laitraAnn@users.noreply.github.com>
2024-11-23 11:59:04 +0300

    Update dott.kt

--- src/main/kotlin/dott.kt
+++ src/main/kotlin/dott.kt
@@ -1,7 +1,1 @@
-import kotlin.math.pow
-import kotlin.math.sqrt
-
-class Dott(x1: Double, x2: Double, x3: Double, y1: Double, y2: Double, y3: Double, st1: Double, st2: Double, st3: Double, per: Double, pl: Double): Triangle(x1, x2, x3, y1, y2, y3){
-    private val rad = pl/per
-    fun znachRad() = "радиус " + "$rad"
-}
+open class Dott(val x: Double, val y: Double)
