c05ea01b58ab586747638729c5589e5c4ca69b05
laitraAnn <162510014+laitraAnn@users.noreply.github.com>
2024-11-23 11:58:52 +0300

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,15 @@
-import java.math.*
-fun main(args: Array<String>) {
-    val treu = Triangle( 1.0,5.0,5.0,0.0, 3.0, 0.0)
+import kotlin.math.*
 
-    println(treu.znach())
+fun main() {
+    val A = Dott(1.0, 0.0)
+    val B = Dott(5.0, 3.0)
+    val C = Dott(5.0, 0.0)
 
+    val triangle = Triangle(A, B, C)
 
+    val center = triangle.Center()
+    val radius = triangle.Radius()
+
+    println("Координаты центра вписанной окружности: (${center.x}, ${center.y})")
+    println("Радиус вписанной окружности: $radius")
 }
