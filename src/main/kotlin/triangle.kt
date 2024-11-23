8219bd60388963aa4f38928d873612b1c5263cdf
laitraAnn <162510014+laitraAnn@users.noreply.github.com>
2024-11-23 11:59:16 +0300

    Update triangle.kt

--- src/main/kotlin/triangle.kt
+++ src/main/kotlin/triangle.kt
@@ -1,17 +1,35 @@
-import kotlin.math.abs
 import kotlin.math.pow
 import kotlin.math.sqrt
 
-open class Triangle (x1: Double, x2: Double, x3: Double, y1: Double, y2: Double, y3: Double){
-    private val st1= sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1))
-    private val st2= sqrt((x2-x3)*(x2-x3) + (y2-y3)*(y2-y3))
-    private val st3= sqrt((x3-x1)*(x3-x1) + (y3-y1)*(y3-y1))
+class Triangle(val A: Dott, val B: Dott, val C: Dott) {
+    private fun side(dot1: Dott, dot2: Dott): Double {
+        return sqrt((dot2.x - dot1.x).pow(2) + (dot2.y - dot1.y).pow(2))
+    }
+    val a = side(A, B)
+    val b = side(B, C)
+    val c = side(C, A)
+    private fun area(): Double {
 
-    private val per= (st1+st2+st3)/2
-    private val pl= abs((x2-x1)*(y3-y1)-(x3-x1)*(y2-y1)) /2
-    open fun znach() {
-        val tochka = Dott(1.0,5.0,5.0,0.0, 3.0, 0.0,st1, st2, st3, per,pl)
-        "радиус " + println(tochka.znachRad())
+        val p = (a + b + c) / 2
+        return sqrt(p * (p - a) * (p - b) * (p - c))
     }
 
+    fun Center(): Dott {
+        val a = side(B, C)
+        val b = side(A, C)
+        val c = side(A, B)
+        val p = a + b + c
+
+        val I_x = (a * A.x + b * B.x + c * C.x) / p
+        val I_y = (a * A.y + b * B.y + c * C.y) / p
+
+        return Dott(I_x, I_y)
+    }
+
+    fun Radius(): Double {
+        val S = area()
+        val p = (a + b + c) / 2
+
+        return S / p
+    }
 }
